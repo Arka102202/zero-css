5049a2f67049ba292070f4682e5d8265192e7821
Arka102202 <arka.online1995@gmail.com>
2024-10-12 11:28:39 +0530

    renamed the file and this will help to create the combinator types class definitions

---
+++ combinators/_child.js
@@ -0,0 +1,33 @@
+import { createClass } from "../addDynamicClass.js";
+import { addMediaQuery} from "../createClass/_generic.js";
+
+export const createSelectorClasses = (className = "", styleTag) => {
+
+
+    // __div[>/ /+/~]p[>/ /+/~].className-[max/min]_[breakpoint]@value1,value2,value3,.....
+
+    className = className.replace(/^__/, "");
+
+    const classParts = className.split("@");
+
+    const selectorNMediaQuery = classParts[0].split("-");
+
+    const classNameToBe = selectorNMediaQuery[0].replace(/_/g, " ");
+
+    const valueParts = classParts.at(-1).split(",");
+
+    let classInside = "";
+    console.log({ valueParts });
+
+    for (let i = 0; i < valueParts.length; i++) {
+        const propValName = valueParts[i];
+        classInside += (createClass(propValName, styleTag, true) + (i !== valueParts.length - 1 ? "\n\t" : "\n"));
+    }
+
+    const classToBuild = `${classNameToBe} {\n${classInside}}`;
+
+    const completeClassDef = selectorNMediaQuery.length === 2 ? addMediaQuery(classToBuild, selectorNMediaQuery) : classToBuild;
+
+    styleTag.innerHTML += completeClassDef;
+
+}
